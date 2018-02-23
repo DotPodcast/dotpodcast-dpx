1a02d774e577f74dfdbb129f05efe0a78319a59e
Mark Steadman <mark@bloomsbury.digital>
2018-02-23 19:12:27 +0000

    Allows the site to deliver SSL-enabled feed URLs if requested via HTTPS

--- dpx/hosting/views/episodes.py
+++ dpx/hosting/views/episodes.py
@@ -16,6 +16,9 @@ class EpisodeListView(PodcastMixin, ListView):
 
         for podcast in Podcast.objects.all()[:1]:
             context['podcast'] = podcast
+            context['feed_head_url'] = podcast.get_feed_head_url(
+                ssl=self.request.is_secure()
+            )
 
         return context
 
